62f8791faa5d0588f60d8779731e7237f6e67123
Henrique Chehad <hchehad@gmail.com>
2015-04-19 22:26:03 -0300

    Fix syntax

--- pysterisk/ami.py
+++ pysterisk/ami.py
@@ -4,81 +4,79 @@
 import socket
 import datetime
 
+
 class Log(object):
 
     def __init__(self, log_file):
         self.log_path = log_file
 
     def log(self, text):
-        open(self.log_path, 'ab+', 512).write("%s: %s\n" % (datetime.datetime.now(), text))
+        open(self.log_path, 'ab+', 512).write("%s: %s\n" %
+                                              (datetime.datetime.now(), text))
 
 
 class AMI():
-	
-	logdata = None
-	s = None
 
-	def __init__(self, log_file='/tmp/pysterisk_ami.log'):
-		self.logdata = Log(log_file)
-		
+    logdata = None
+    s = None
 
-	def connect(self, host, port, username, password,):
-		self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-		self.s.connect((host, port))
+    def __init__(self, log_file='/tmp/pysterisk_ami.log'):
+        self.logdata = Log(log_file)
 
-		params = [
-					"Action: login",
-        			"Events: off",
-          			"Username: " + username,
-          			"Secret: " + password
-          		]
+    def connect(self, host, port, username, password,):
+        self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.s.connect((host, port))
 
-		self.s.send("\r\n".join(params) + "\r\n")
+        params = [
+            "Action: login",
+            "Events: off",
+            "Username: " + username,
+            "Secret: " + password
+        ]
 
-		# Login try respose
-		data = ""
-		while "\r\n" not in data:
-    			data += self.s.recv(1024)
+        self.s.send("\r\n".join(params) + "\r\n")
 
-		self.s.send("Action: status\r\n\r\n")
+        # Login try respose
+        data = ""
+        while "\r\n" not in data:
+            data += self.s.recv(1024)
 
-		# Action Respose
-		return self._get()
+        self.s.send("Action: status\r\n\r\n")
 
-	def logoff(self):
-		self.logdata.log('Action Logoff')
-		self.s.send("Action: Logoff\r\n\r\n")
-		self.s.close()
+        # Action Respose
+        return self._get()
 
-		
-	def command(self, command=None):
+    def logoff(self):
+        self.logdata.log('Action Logoff')
+        self.s.send("Action: Logoff\r\n\r\n")
+        self.s.close()
 
-		params = ["Action: command",]
-		params.append("command: " + command)
-		self.s.send("\r\n".join(params) + "\r\n\r\n")
-		
-		self.logdata.log('Action Command: %s' % command)		
-		return self._get()
+    def command(self, command=None):
 
+        params = ["Action: command", ]
+        params.append("command: " + command)
+        self.s.send("\r\n".join(params) + "\r\n\r\n")
 
-	def action(self, action=None, message=None, value=None):
+        self.logdata.log('Action Command: %s' % command)
+        return self._get()
 
-		params = ["Action: " + action,]
-		if (message):
-			params.append(message + ": " + value)
+    def action(self, action=None, message=None, value=None):
 
-		self.s.send("\r\n".join(params) + "\r\n\r\n")
-		
-		self.logdata.log('Action %s - Message: %s - Value: %s' % (action, message, value))
-		return self._get()
+        params = ["Action: " + action, ]
+        if (message):
+            params.append(message + ": " + value)
 
+        self.s.send("\r\n".join(params) + "\r\n\r\n")
 
-	def _get(self):
-		data = ""
-		while (True):
-			if ('\r\n\r\n' in data):
-				break
-			data += self.s.recv(1024)
-			print data
-			return data
+        self.logdata.log('Action %s - Message: %s - Value: %s' %
+                         (action, message, value))
+        return self._get()
 
+    def _get(self):
+        data = ""
+        while (True):
+            if ('\r\n\r\n' in data):
+                break
+            data += self.s.recv(1024)
+            print data
+            return data
